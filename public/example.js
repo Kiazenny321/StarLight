2b80d40d7d505e09582ab29d126d19ab669e737b
Bwlake <bwigal2@nastudents.org>
2024-04-02 17:44:23 -0400

    Update example.js

--- public/example.js
+++ public/example.js
@@ -4,11 +4,13 @@ document // makes it so you can press enter to submit as opposed to just being a
         if (event.key === "Enter") {
             event.preventDefault();
             document.getElementById("searchButton").click();
+            window.location.href = window.location.href + "go.html";
         }
     });
 
 document.getElementById("searchButton").onclick = function (event) {
     event.preventDefault();
+    window.location.href = window.location.href + "go.html";
 
     let url = document.getElementById("urlInput").value; // if no periods are detected in the input, search google instead
     let searchUrl = "https://www.google.com/search?q=";
@@ -20,6 +22,6 @@ document.getElementById("searchButton").onclick = function (event) {
             url = "https://" + url;
         }
     }
-
+localStorage.setItem("Iframe", __uv$config.prefix + __uv$config.encodeUrl(url););
     iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 };
