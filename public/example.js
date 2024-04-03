803ca631be8c92bc4f9317f9cb1c0dfe15091cfa
Bwlake <152343852+Bwlake@users.noreply.github.com>
2024-04-03 22:30:06 +0000

    fixing error where search doesnt work

--- public/example.js
+++ public/example.js
@@ -1,54 +1,4 @@
-// About:Blank Maker
-let about = 1
-if (about == 1) {
-  let inFrame
-try {
-  inFrame = window !== top
-} catch (e) {
-  inFrame = true
-}
 
-if (!inFrame && !navigator.userAgent.includes('Firefox')) {
-  const popup = open('about:blank', '_blank')
-  if (!popup || popup.closed) {
-    alert('Please allow popups and redirects.')
-  } else {
-    const doc = popup.document
-    const iframe = doc.createElement('iframe')
-    const style = iframe.style
-    const link = doc.createElement('link')
-
-    const name = localStorage.getItem('name') || 'My Drive - Google Drive'
-    const icon = localStorage.getItem('icon') || 'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png'
-
-    doc.title = name
-    link.rel = 'icon'
-    link.href = icon
-
-    iframe.src = location.href
-    style.position = 'fixed'
-    style.top = style.bottom = style.left = style.right = 0
-    style.border = style.outline = 'none'
-    style.width = style.height = '100%'
-
-    doc.head.appendChild(link)
-    doc.body.appendChild(iframe)
-
-    const pLink = localStorage.getItem(encodeURI('pLink')) || 'https://www.nasa.gov/'
-    location.replace(pLink)
-
-    const script = doc.createElement('script')
-    script.textContent = `
-      window.onbeforeunload = function (event) {
-        const confirmationMessage = 'Leave Site?';
-        (event || window.event).returnValue = confirmationMessage;
-        return confirmationMessage;
-      };
-    `
-    doc.head.appendChild(script)
-  }
-}
-}
 // Loading Page
 document // makes it so you can press enter to submit as opposed to just being able to press a button
     .getElementById("urlInput")
@@ -72,7 +22,7 @@ document.getElementById("searchButton").onclick = function (event) {
             url = "https://" + url;
         }
     }
-
+    setTimeout(() => {  console.log('World!'); }, 1000);
       localStorage.setItem("Iframe", __uv$config.prefix + __uv$config.encodeUrl(url));
       window.location.href = "https://star-light-nine.vercel.app/go.html"; 
 };
