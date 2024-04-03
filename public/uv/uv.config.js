15d87c473711f9214a637672ca9b452c3f6d07a8
Bwlake <bwigal2@nastudents.org>
2024-04-03 15:59:16 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
+    prefix: '/uv/service/',
+    bare: 'https://focus-bare.vercel.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
