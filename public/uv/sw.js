26c4da69726094f3acb211e5071939479e7cba9e
Bwlake <bwigal2@nastudents.org>
2024-04-03 20:44:04 -0400

    Trying to fix UV

--- public/uv/sw.js
+++ public/uv/sw.js
@@ -11,7 +11,7 @@ importScripts(__uv$config.sw || '/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => 
-  if (event.request.url.startsWith(location.orgin + __uv$config.prefix))
-event.respondWith(sw.fetch(event)));
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.startsWith(location.origin + __uv$config.prefix))
+        return event.respondWith(sw.fetch(event));
 });
